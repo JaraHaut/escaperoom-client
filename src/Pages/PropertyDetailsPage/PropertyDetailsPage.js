2a19ce26e183b8d13fe44a66f09042edd382c391
Jara Haut <jara.haut@gmail.com>
2023-08-27 12:36:28 +0100

    create property details page

---
+++ src/Pages/PropertyDetailsPage/PropertyDetailsPage.js
@@ -0,0 +1,11 @@
+import "./PropertyDetailsPage.scss";
+function PropertyDetailsPage() {
+  return (
+    <>
+      <Header />
+      <Property />
+      <Review />
+    </>
+  );
+}
+export default PropertyDetailsPage;
--- src/Pages/PropertyPage/PropertyPage.js
+++
@@ -1,5 +0,0 @@
-import "./PropertyPage.scss";
-function PropertyPage() {
-  return <></>;
-}
-export default PropertyPage;
