92c33c123b0783301def6ca32857c569dbc214b1
Jara Haut <jara.haut@gmail.com>
2023-08-31 00:25:00 +0100

    add stylng to the property card component, add colors to variables

--- src/Components/PropertyCard/PropertyCard.js
+++ src/Components/PropertyCard/PropertyCard.js
@@ -7,27 +7,39 @@ function PropertyCard({ properties }) {
   }
   return (
     <>
-      <div>
+      <main className="property-list">
         {properties.map((property) => {
           return (
             <>
               {" "}
               <Link to={`/properties/${property.id}`} key={property.id}>
-                <section className="property-card">
-                  <p>{property.title} </p>
-                  <p>Post Code: {property.postcode}</p>
-                  <p>Letting Agent: {property.agency}</p>
-                  <p>No. bedrooms: {property.bedrooms}</p>
-                  <p>{property.id}</p>
-                  <div>
-                    <img src={property.picture} alt="apartment" />
+                <section
+                  className="property-card"
+                  style={{ backgroundImage: `url(${property.picture})` }}
+                >
+                  <div className="property-card__data-wrapper">
+                    <h2 className="property-card__title">{property.title} </h2>
+                    <p className="property-card__data">
+                      Post Code: {property.postcode}
+                    </p>
+                    <p className="property-card__data">
+                      Letting Agent: {property.agency}
+                    </p>
+                    <p className="property-card__data">
+                      No. bedrooms: {property.bedrooms}
+                    </p>
+                    <p className="property-card__data">{property.id}</p>
+                    <p className="property-card__data">Property Rating </p>
                   </div>
+                  {/* <div className="property-card__img">
+                    <img src={property.picture} alt="apartment" />
+                  </div> */}
                 </section>
               </Link>
             </>
           );
         })}
-      </div>
+      </main>
     </>
   );
 }
